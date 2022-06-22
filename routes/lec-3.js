18ab3a54f0e6e5449a97f76923a3537579733dc9
ShadowPrince <zhek4770@gmail.com>
2022-06-22 21:03:00 +0300

    Init lec-3 diminutive

--- routes/lec-3.js
+++ routes/lec-3.js
@@ -1,14 +1,18 @@
 var express = require("express");
 var router = express.Router();
-const nouns = require('../data/nouns');
-const adjectives = require('../data/adjectives');
+const nouns = require("../data/nouns");
+const adjectives = require("../data/adjectives");
 
 router.get("/nouns", function (req, res, next) {
-  res.render("lec-3/nouns", {title: "Lec-3: Nouns"});
+  res.render("lec-3/nouns", { title: "Lec-3: Nouns" });
 });
 
 router.get("/nouns-plural", function (req, res, next) {
-  res.render("lec-3/nouns-plural", {title: "Lec-3: Nouns"});
+  res.render("lec-3/nouns-plural", { title: "Lec-3: Nouns" });
+});
+
+router.get("/diminutive", function (req, res, next) {
+  res.render("lec-3/diminutive", { title: "Lec-3: Nouns" });
 });
 
 module.exports = router;
